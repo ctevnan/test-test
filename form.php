991db01342079b151bbff5ebea063b678f57dec0
ctevnan <carolyntevnan@yahoo.com>
2016-07-21 20:05:58 -0400

    writing out form.php

---
+++ form.php
@@ -0,0 +1,9 @@
+<?php
+$connection = mysql_connect("localhost", "root", "");
+$db = mysql_select_db("mydba", $connection);
+$method2=$_POST['method1'];
+$userID2=$_POST['userID1'];
+$currentDB2=$_POST['currentDB1'];
+$mode2=$_POST['mode1'];
+$halberdApiUrl2=$_POST['halberdApiUrl1'];
+$query = mysql_query("insert into form_element(method, userID, )
---
+++ js/form.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+$("#submit").click(function() {
+var method = $("method").val();
+var userID = $("#userID").val();
+var currentDB = $("currentDB").val();
+var mode = $("mode").val();
+var halberdApiUrl = "http://matchapi.halberdtechnologies.com/api/Database?userID=TestFolder&database=VPNVerify888&mode=match";
+if (method == '' || userID == '' || currentDB == '' || mode == '' || halberdApiUrl == '') {
+  alert("Insertion failed. Some fields are empty.");
+} else {
+//returns successful data submission message when the entered info is stored in db
+$.post("form.php", {
+method1: method,
+userID1: userID,
+currentDB1: currentDB,
+mode1: mode,
+halberdApiUrl1: halberdApiUrl
+}, function(data) {
+alert(data);
+$('#form')[0].reset(); //reset form fields
+});
+}
+});
+});  
